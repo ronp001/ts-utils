5abf0c853d1b67c8e943641453253d38c66fe72e
Ron Perry <ronp001@gmail.com>
2018-10-22 00:44:58 +0300

    fix for CliApp

--- src/cli-app.ts
+++ src/cli-app.ts
@@ -1,14 +1,16 @@
-import * as program from 'commander' 
+import * as program from 'commander'
 
 export abstract class CliApp {
     protected abstract _init(): void;
 
-    private _did_exec_cmd = false   
-    
+    protected program = program;
+
+    private _did_exec_cmd = false
+
     // utility to generate action functions to be
     // called from commander
-    protected action(func : (...args:any[])=>void) : (...args:any[])=>void {
-        let newfunc = (...args:any[]) => {
+    protected action(func: (...args: any[]) => void): (...args: any[]) => void {
+        let newfunc = (...args: any[]) => {
             this._did_exec_cmd = true
             this.beforeCommand.apply(this)
             func.apply(this, args)
@@ -18,7 +20,7 @@ export abstract class CliApp {
     }
 
     // canonize the input received from commander's variadic args mechanism
-    protected fix(arg1:string|undefined, arg2:string[]|undefined) : string[] {
+    protected fix(arg1: string | undefined, arg2: string[] | undefined): string[] {
         if (typeof arg1 != "undefined" && arg2 && arg2.length > 0) {
             return [arg1].concat(arg2)
         } else if (!!arg1) {
@@ -28,8 +30,8 @@ export abstract class CliApp {
     }
 
     // override to get access to the args before any command is executed
-    protected beforeCommand() {}
-    protected afterCommand() {}
+    protected beforeCommand() { }
+    protected afterCommand() { }
 
     public main() {
 
@@ -37,15 +39,15 @@ export abstract class CliApp {
 
         try {
             program.parse(process.argv)
-            if ( !this._did_exec_cmd ) {
+            if (!this._did_exec_cmd) {
                 program.help()
             }
-        } catch(e) {
-            if ( program.verbose ) {
+        } catch (e) {
+            if (program.verbose) {
                 console.log(e)
             } else {
                 console.log(e.message)
             }
-        } 
+        }
     }
 }
